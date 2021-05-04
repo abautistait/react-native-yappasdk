462ba593c0bef6c3203ac99051093183a3e06b2d
Axel Bautista <abautista@intermediait.com>
2021-05-04 17:46:45 -0300

    Updated iOS native methods

--- ios/Yappasdk.swift
+++ ios/Yappasdk.swift
@@ -1,8 +1,37 @@
+import YappaSDK
+
 @objc(Yappasdk)
 class Yappasdk: NSObject {
 
-    @objc(multiply:withB:withResolver:withRejecter:)
-    func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
-        resolve(a*b)
+    @objc(initialize:withAppId:)
+    func initialize(hash: String, appId: String) -> Void {
+        Yappa.initialize(hash: hash, appId: appId)
+    }
+
+    @objc(setContentUrl:)
+    func setContentUrl(url: String) {
+        Yappa.setSiteUrl(url)
+    }
+    
+    @objc(setCallbackScheme:)
+    func setCallbackScheme(urlScheme: String) {
+        Yappa.setCallbackScheme(urlScheme)
+    }
+    
+    @objc(setContentId:)
+    func setContentId(contentId: String) {
+        Yappa.setContentId(contentId)
+    }
+    
+    @objc(show)
+    func show() {
+        DispatchQueue.main.async {
+            Yappa.openSDK();
+        }
+    }
+
+    @objc(close)
+    func close() {
+        
     }
 }
