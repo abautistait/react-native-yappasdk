1b3f785d3ca753c13acea8f2342026f860e8fa7b
Maxi Cassola <>
2021-05-04 17:14:32 -0300

    update iOS interface

--- ios/Yappasdk.m
+++ ios/Yappasdk.m
@@ -2,8 +2,16 @@
 
 @interface RCT_EXTERN_MODULE(Yappasdk, NSObject)
 
-RCT_EXTERN_METHOD(multiply:(float)a withB:(float)b
-                 withResolver:(RCTPromiseResolveBlock)resolve
-                 withRejecter:(RCTPromiseRejectBlock)reject)
+RCT_EXTERN_METHOD(initialize:(NSString *)hash withAppId:(NSString *)appId)
+
+RCT_EXTERN_METHOD(setContentUrl:(NSString *)url)
+
+RCT_EXTERN_METHOD(setCallbackScheme:(NSString *)urlScheme)
+
+RCT_EXTERN_METHOD(setContentId:(NSString *)contentId)
+
+RCT_EXTERN_METHOD(show)
+
+RCT_EXTERN_METHOD(close)
 
 @end
